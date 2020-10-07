6f62e226e7cba8b87b5623cb62fd602aa2298474
Ahmad Husen <husenisme@gmail.com>
2020-10-07 21:45:13 +0700

    Add helmet

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import helmet from 'helmet';
 
 const app = express();
 const PORT = 8080;
+
+app.use(helmet());
 
 app.get('/', (req, res) => res.send('Awesome Express + Typescript Server'));
 app.listen(PORT, () => {
