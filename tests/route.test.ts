2c5018de980c5b4ba77f5b8beb47ff5c82ef7673
Ahmad Husen <husenisme@gmail.com>
2020-10-16 22:30:22 +0700

    Fix lint for route test

--- tests/route.test.ts
+++ tests/route.test.ts
@@ -1,19 +1,17 @@
-import request from 'supertest'
-import app from '../server'
+import request from 'supertest';
+import app from '../server';
 
 describe('Index route test', () => {
-	it('Should pass index route', async done => {
-    const res = await request(app)
-      .get('/')
-    expect(res.status).toEqual(200)
-		expect(res.body.message).toBe('Yo!')
-		done()
-	})
-	
-	it('Should failed access index route', async done => {
-    const res = await request(app)
-      .post('/')
-    expect(res.status).toEqual(404)
-		done()
-  })
+	it('Should pass index route', async (done) => {
+		const res = await request(app).get('/');
+		expect(res.status).toEqual(200);
+		expect(res.body.message).toBe('Yo!');
+		done();
+	});
+
+	it('Should failed access index route', async (done) => {
+		const res = await request(app).post('/');
+		expect(res.status).toEqual(404);
+		done();
+	});
 });
