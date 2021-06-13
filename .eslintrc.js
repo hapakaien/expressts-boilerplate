098d5c27e8aaf9b90c11e67da59f2cb5b2ff4897
Ahmad Husen <hi@husen.id>
2021-06-13 21:29:43 +0700

    chore: change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
   root: true,
   env: {
-    es2021: true,
     node: true,
+    es2020: true,
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    project: './tsconfig.json',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
   extends: [
-    'eslint:recommended',
+    'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:prettier/recommended',
   ],
   rules: {},
